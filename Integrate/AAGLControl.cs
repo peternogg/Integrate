f9e2310d50f78dacfcfc2d1f3f8991f5b04df20d
Peternogg <peternogg@gmail.com>
2014-03-22 21:37:17 -0700

    I forgot to add the most important thing
    Integrate (the project) was referencing something in my documents, which
    wasn't included in the git system so I added it
    
    Outta work better now :V

---
+++ Integrate/AAGLControl.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+using OpenTK.Graphics;
+
+namespace AAGLControlLib
+{
+    public partial class AAGLControl : OpenTK.GLControl
+    {
+        public AAGLControl()
+            : base(new GraphicsMode(32, 24, 8, 4), 3, 0, GraphicsContextFlags.ForwardCompatible)
+        {
+            InitializeComponent();
+        }
+    }
+}
